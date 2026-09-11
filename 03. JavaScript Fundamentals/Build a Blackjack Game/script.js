ac227507c5feca074f3b193af223900ce5957f8a
Ndumiso Mguni <Ndumiso29@gmail.com>
2026-09-10 21:59:36 -0400

    calculator challange complete and blackjack game refactoring started

--- 03. JavaScript Fundamentals/Build a Blackjack Game/script.js
+++ 03. JavaScript Fundamentals/Build a Blackjack Game/script.js
@@ -1,17 +1,67 @@
 const firstCard = Math.floor(Math.random() * 11) + 2
 const secondCard = Math.floor(Math.random() * 11) + 2
 
-let sum = firstCard + secondCard
 
 
-const blackJack = (sum) => {
-   if (sum < 21) {
-      return "Do you want to draw a new card"
+const startGameBtn = document.querySelector('#startGame')
+const messageEl = document.querySelector('#message-el')
+const sumEl = document.querySelector('#sum-el')
+const cardsEl = document.querySelector('#cards-el')
+const newCardBtn = document.querySelector('#newcard-el')
+
+
+startGameBtn.addEventListener('click', startGame)
+newCardBtn.addEventListener('click', newCards)
+
+// let firstCard = 10;
+// let secondCard = 4;
+let sum = firstCard + secondCard;
+// let thirdCardSum = sum + thirdCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+
+
+
+
+
+function startGame() {
+
+   sumEl.textContent = `Sum: ${sum}`
+   cardsEl.textContent = `Cards: ${firstCard} • ${secondCard}`
+
+
+   if (sum <= 20) {
+      message = "Do you want to draw a new card?"
+
    } else if (sum === 21) {
-      return "You've got Blackjack!"
-   } else if (sum > 21) {
-      return "You're out of the game!"
+      message = "You've got Blackjack!"
+      hasBlackJack = true
+   } else {
+      message = "You're out of the game!"
+      isAlive = false
    }
+
+   messageEl.classList.add('focus')
+   messageEl.textContent = message
 }
 
-blackJack(sum)
+
+
+function newCards() {
+   // console.log("Drawing a new card from the deck!")
+
+
+   // const newCard = Math.floor(Math.random() * 11) + 2
+
+   sum += newCard
+   // cardsEl.textContent = `Cards: ${firstCard} • ${secondCard}  • ${newCard}`
+
+   startGame()
+
+
+
+
+}
+
